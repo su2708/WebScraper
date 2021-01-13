b39d4cea53078c55b992bac0edfd562b6d3ad97e
su2708 <ysy2708@gmail.com>
2021-01-13 11:27:01 +0000

    Update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import requests
-import beautifulsoup
 
 indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
 
