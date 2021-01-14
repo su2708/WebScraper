9218a92ce69d4166ee225da299109643347beb37
su2708 <ysy2708@gmail.com>
2021-01-14 13:28:26 +0000

    Update

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 
 indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
 
-print(indeed_result.text)
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class":"pagination"})
+
+pages = pagination.find_all('a')
+spans = []
+for page in pages:
+  spans.append(page.find("span"))
+spans = spans[:-1]
