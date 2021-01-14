cc2353b9df21d0a8a2f70e621ab30ccb4bb19efd
su2708 <ysy2708@gmail.com>
2021-01-14 13:38:45 +0000

    Update

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-indeed_result = requests.get("https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&salary=&radius=25&l=&fromage=any&limit=50&sort=&psf=advsrch&from=advancedsearch")
+indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class":"pagination"})
 
-pages = pagination.find_all('a')
-spans = []
-for page in pages:
-  spans.append(page.find("span"))
-spans = spans[:-1]
+links = pagination.find_all('a')
+pages = []
+for link in links[:-1]:
+  pages.append(int(link.string))
+
+max_page = pages[-1]
